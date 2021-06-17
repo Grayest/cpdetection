6bf2b1df0f9686c57ae451a303e212daa2ddc5e9
Mansi <mansi142002@gmail.com>
2021-06-17 18:28:46 +0530

    Added files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h3 class="heading">Live Object Detector</h3>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
